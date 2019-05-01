814ff3118df4e097e41c1b115a9a574790aff5e0
frankdin1 <50062629+frankdin1@users.noreply.github.com>
2019-04-30 21:58:33 -0500

    Add files via upload

---
+++ Detecting_peak_times__on_TFT.ino
@@ -0,0 +1,181 @@
+/*
+ * Code written by Frank Din for UTA Senior Design Project
+ * */
+ #include <TFT.h>  // Arduino LCD library
+ #include <SPI.h>
+/*// SPI pin definition for the Uno
+#define cs   10
+#define dc   9
+#define rst  8
+#define mosi  11
+#define sclk  13
+*/
+// SPI pin definition for the Mega. Check the SPI page on Arduino website for more info
+#define cs   10
+#define dc   9
+#define rst  8
+#define sda  51
+#define scl  52//not the sd card section
+TFT screen = TFT(cs, dc, rst);// create an instance of the screen library
+unsigned long peaktArr[11], pAndt[2][3]; //peaktArr stores the peak values from pAndt; pAndt stores the values from the sensor which are compared to one another to determine a peak
+ float tdiff[10];//array to store time difference between consecutive time stamps
+  int i = 0;
+  int j = 0;
+  int k = 0;
+  float inst_f = 0;
+  int p = 0;
+  int a = 0;
+  unsigned long calb;
+  int xPos = 0;
+void setup(){
+  Serial.begin(9600);
+  screen.begin();
+  screen.setTextSize(2);
+  screen.background(0, 0, 255);
+  unsigned long delayTime = millis();
+  while(millis() - delayTime <= 3000){
+  screen.setCursor(0,0);
+  screen.print("Welcome To");
+  screen.setCursor(0,25);
+  screen.print("Chelsea's");
+  screen.setCursor(0,50);
+  screen.print("Heart Rate");
+  screen.setCursor(0,75);
+  screen.print("Device");
+  }
+  screen.background(0,0,255);
+  delayTime = millis();
+  while(millis() - delayTime <= 3000){
+  screen.setCursor(0,0);
+  screen.print("Place Finger");
+  screen.setCursor(0,25);
+  screen.print("On Sensor");
+  }
+  screen.background(0,0,255);
+  screen.setCursor(0,50);
+  screen.print("Calibrating");
+  /*delayTime = millis();
+  while(millis() - delayTime <=5000){
+    screen.setCursor(0,50);
+    screen.print("Calibrating");
+    unsigned long delayTime = millis();
+    while(millis() - delayTime <=1000){
+      unsigned long delayTime = millis();
+      calb = analogRead(A0);
+      while(millis() - delayTime <=200){
+      }
+    }
+    screen.background(0,0,255);
+  }*/
+  for(i = 0; i < 3; i++){//read and store 3 values at a time in pAndt   
+      unsigned long delayTime = millis();//
+      pAndt[0][i] = analogRead(A0);//1st row of pAndt stores the sensor values
+      pAndt[1][i] = millis();// 2nd row of pAndt stores the time stamp of the sensor values
+      while (millis() - delayTime  <=200){//delay in ms before reading the next value
+      }
+    }
+    /*for(i = 1; i < 2; i++){   
+      if(hp <= pAndt[0][i]){
+        hp = pAndt[0][i];
+      }
+    }*/
+    for(i = 1; i < 2; i++){   
+      if(pAndt[0][i]>pAndt[0][i-1] && pAndt[0][i]>pAndt[0][i+1] /*&& pAndt[0][i] >=0.3*hp*/){ //compare the 3 values in pAndt to determine a peak
+          peaktArr[0] = pAndt[1][i];// store the value of i that satisfies the peak condition, and do the comparison above until j is 10
+      }
+    }
+    
+  while(j<11){
+    pAndt[0][0] = pAndt[0][1];
+    pAndt[1][0] = pAndt[1][1];
+
+    pAndt[0][1] = pAndt[0][2];
+    pAndt[1][1] = pAndt[1][2];
+    unsigned long delayTime = millis();//
+    for(i = 2; i < 3; i++){//read and store 1 value at a time in pAndt   
+      pAndt[0][i] = analogRead(A0);//1st row of pAndt stores the sensor values
+      pAndt[1][i] = millis();// 2nd row of pAndt stores the time stamp of the sensor values
+      while (millis() - delayTime  <=200){//delay in ms before reading the next value
+      }
+    }
+    for(i = 1; i < 2; i++){   
+      if(pAndt[0][i]>pAndt[0][i-1] && pAndt[0][i]>pAndt[0][i+1]){ //compare the 3 values in pAndt to determine a peak
+          peaktArr[j++] = pAndt[1][i];// store the value of i that satisfies the peak condition, and do the comparison above until j is 10
+      }
+    }
+  }
+    for(i = 0; i < 10; i++){
+        tdiff[k++] = (((float)peaktArr[i+1] - (float)peaktArr[i])/1000);
+    }
+     for(i = 0; i < 10; i++){
+        inst_f = inst_f + (1/tdiff[i]);
+    }
+}
+
+void loop(){
+ int xPos = 0;
+ float inst_f = 0;
+ for(i = 2; i<11; i++){
+  peaktArr[i-2] = peaktArr[i];
+  }
+  j = 9;
+
+  for(i = 1; i<10; i++){
+  tdiff[i-1] = tdiff[i];
+  }
+  while(j<11){
+    pAndt[0][0] = pAndt[0][1];
+    pAndt[1][0] = pAndt[1][1];
+
+    pAndt[0][1] = pAndt[0][2];
+    pAndt[1][1] = pAndt[1][2];
+    unsigned long delayTime = millis();//
+    for(i = 2; i < 3; i++){//read and store 1 value at a time in pAndt   
+      pAndt[0][i] = analogRead(A0);//1st row of pAndt stores the sensor values
+      pAndt[1][i] = millis();// 2nd row of pAndt stores the time stamp of the sensor values
+      while (millis() - delayTime  <=100){//delay in ms before reading the next value
+      }
+    }
+    /*for(i = 1; i < 2; i++){   
+      if(hp <= pAndt[0][i]){
+        hp = pAndt[0][i];
+      }
+    }*/
+    for(i = 1; i < 2; i++){   
+      if(pAndt[0][i]>pAndt[0][i-1] && pAndt[0][i]>pAndt[0][i+1] /*&& pAndt[0][i] >= 0.3*hp*/){ //compare the 3 values in pAndt to determine a peak
+          peaktArr[j++] = pAndt[1][i];// store the value of i that satisfies the peak condition, and do the comparison above until j is 10
+      }
+    }
+  }
+    tdiff[9] = (((float)peaktArr[10] - (float)peaktArr[9])/1000);
+     for(i = 0; i < 10; i++){
+        inst_f = inst_f + (1/tdiff[i]);
+    }
+    screen.background(0,0,255);
+    screen.setCursor(0,0);//the 1st value in brackets sets the distance from the left edge of the screen, and the 2nd value sets the distance from the top edge of the screen
+    screen.print("Heart rate ");
+    screen.setCursor(0,20);
+    screen.print((inst_f/10)*60);
+    screen.setCursor(75,20);
+    screen.println(" bpm");
+
+    
+    //unsigned long displayTime = millis();
+    //while(millis() - displayTime <=5000){ // The waveform will display for 5 seconds 
+    while(xPos<=160){
+    int sensor = analogRead(A0); 
+    int graphHeight = map(sensor,0,682,5,screen.height()/1.5);
+    screen.stroke(255,255,255);
+    //xPos*2 causes the waveform to reach the end of the screen after 5 s
+    screen.line(xPos, (screen.height() - graphHeight), xPos, screen.height() - graphHeight+3); //screen.height() - graphHeight + 5 affects the thickness of the line. The bigger the number you add to screen.height() - graphHeight is, the thicker the line will be
+    xPos++;
+    unsigned long delayTime = millis();
+    while(millis() - delayTime<=25){
+    }
+ }
+  screen.background(0,0,255);//1st value is blue, 2nd value is green and 3rd value is red
+  screen.print("Calculating..");
+  unsigned long delayTime = millis();
+  //while (millis()- delayTime <= ){
+  //}
+}
